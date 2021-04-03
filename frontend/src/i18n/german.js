ce668cff48212ab6ef6b7d6859f2114bcb85ef97
Clemens Prosser <clemens.prosser@student.uibk.ac.at>
2021-04-03 14:18:07 +0200

    Add navbar component

--- frontend/src/i18n/german.js
+++ frontend/src/i18n/german.js
@@ -1,3 +1,5 @@
 export default {
-
+    generic: {
+        overview: 'Übersicht',
+    },
 };
--- frontend/src/router/Wrapper.vue
+++ frontend/src/router/Wrapper.vue
@@ -1,3 +1,21 @@
 <template>
-    <router-view />
+    <div>
+        <navbar v-show="showNavbar" />
+        <router-view />
+    </div>
 </template>
+<script>
+import Navbar from '@/components/page/Navbar.vue';
+
+export default {
+    name: 'Wrapper',
+    components: {
+        Navbar,
+    },
+    computed: {
+        showNavbar() {
+            return this.$route.meta.navbar == null ? true : this.$route.meta.navbar;
+        },
+    },
+};
+</script>
