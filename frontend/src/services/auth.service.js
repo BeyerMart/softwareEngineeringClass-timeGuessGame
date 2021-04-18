215b315f282380a9da297a9c3b8a373dfe6c19a5
Islam Mechtijev <islam.mechtijev@student.uibk.ac.at>
2021-04-18 12:09:55 +0200

    Add login/signup pages & components + update translations + add routes

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = '/api/auth';
+
+export function login(loginData) {
+    return axios.post(`${API_URL}/signin`, {
+        username: loginData.username,
+        password: loginData.password,
+    }).then((response) => {
+        if (response.data.token) {
+            localStorage.setItem('token', response.data.token);
+            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+        }
+        return response;
+    });
+}
+export function getCurrentUser() {
+    return axios.get(`${API_URL}/me`);
+}
+
+export function logout() {
+    localStorage.removeItem('token');
+}
