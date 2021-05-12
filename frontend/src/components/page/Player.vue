ab14ec669500fe9b089205df6a9bf0b1da5c9605
Martin Fritz Neuner <martin.neuner@student.uibk.ac.at>
2021-05-12 22:07:17 +0200

    Init Player Component

---
+++ frontend/src/components/page/Player.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="w-4/5 h-10 py-3 px-1">
+        <p class="hover:text-blue-dark">
+            {{ player.username }}
+        </p>
+        <p class="text-sm text-grey-dark">
+            <span
+                v-if="player.virtual_id"
+                class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"
+            >Virtual User</span>
+            <span
+                v-for="(badge, index) in badges"
+                :key="index"
+                :class="getClass(badge.colour)"
+            >badge.text</span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Player',
+    props: {
+        player: {
+            required: true,
+        },
+        badges: {
+            required: false,
+            type: Array,
+            default: [],
+        },
+    },
+    methods: {
+        getClass(colour) {
+            return `inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-${colour}-100 bg-${colour}-600 rounded-full`
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/components/page/VirtualTeam.vue
+++ frontend/src/components/page/VirtualTeam.vue
@@ -1,11 +1,66 @@
 <template>
-
+    <div class="w-full max-w-screen-xl mx-auto p-6">
+        <div class="relative rounded overflow-hidden border border-grey-light mb-8 bg-white">
+            <div class="border-b border-grey-light p-4 bg-grey-lighter py-8">
+                <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+                    <div class="sm:flex sm:items-center px-2 py-4">
+                        <div class="flex-grow">
+                            <h3 class="font-normal px-2 py-3 leading-tight">
+                                Contacts
+                            </h3>
+                            <div class="w-full">
+                                <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
+                                    <div
+                                        v-for="player in players"
+                                        :key="player"
+                                    >
+                                        <Player
+                                            :player="player"
+                                            :badges="hostId && player.id === hostId ? [{text: 'host', colour: 'green'}] : null"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sm:flex bg-grey-light sm:items-center px-2 py-4">
+                        <div class="flex-grow text-right">
+                            <button
+                                class="bg-blue hover:bg-blue-dark text-white py-2 px-4 rounded"
+                                @click="$emit('joinTeam')"
+                            >
+                                Join
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Player from '@/components/page/Player';
+
 export default {
-    name: "VirtualTeam"
-}
+    name: 'VirtualTeam',
+    components: { Player },
+    props: {
+        team: {
+            required: true,
+            type: Object,
+        },
+        hostId: {
+            default: null,
+            required: false,
+        },
+    },
+    computed: {
+        players() {
+            return this.team.players();
+        },
+    },
+};
 </script>
 
 <style scoped>
