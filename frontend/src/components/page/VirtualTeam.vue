0d06a9bd2cbad25ee657f42159f850f2fe2b8dec
Islam Mechtijev <islam.mechtijev@student.uibk.ac.at>
2021-05-13 21:46:20 +0200

    Move waitForPlayers to room.vue

--- frontend/src/components/page/VirtualTeam.vue
+++ frontend/src/components/page/VirtualTeam.vue
@@ -24,7 +24,10 @@
             </div>
         </div>
 
-        <div class="leading-6 space-y-1 w-full">
+        <div
+            v-show="!isGameplay"
+            class="leading-6 space-y-1 w-full"
+        >
             <p>
                 <button
                     class="bg-gray-400 text-white py-2 px-4 outline-none rounded w-full mt-5"
@@ -35,45 +38,6 @@
             </p>
         </div>
     </div>
-
-    <!-- <div class="w-full max-w-screen-xl mx-auto p-6">
-        <div class="relative rounded overflow-hidden border border-grey-light mb-8 bg-white">
-            <div class="border-b border-grey-light p-4 bg-grey-lighter py-8">
-                <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-                    <div class="sm:flex sm:items-center px-2 py-4">
-                        <div class="flex-grow">
-                            <h3 class="font-normal px-2 py-3 leading-tight">
-                                {{ team.name }}
-                            </h3>
-                            <div class="w-full">
-                                <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
-                                    <div
-                                        v-for="(player, index) in players"
-                                        :key="index"
-                                    >
-                                        <Player
-                                            :player="player"
-                                            :badges="hostId && player.id === hostId ? [{text: 'host', colour: 'green'}] : null"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sm:flex bg-grey-light sm:items-center px-2 py-4">
-                        <div class="flex-grow text-right">
-                            <button
-                                class="bg-blue hover:bg-blue-dark text-white py-2 px-4 rounded"
-                                @click="$emit('joinTeam')"
-                            >
-                                Join
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </template>
 
 <script>
@@ -92,7 +56,7 @@ export default {
             required: false,
             type: Number,
         },
-        isGamePlay: {
+        isGameplay: {
             default: false,
             type: Boolean,
         },
--- frontend/src/pages/game/Room.vue
+++ frontend/src/pages/game/Room.vue
@@ -49,7 +49,10 @@
                         </div>
                     </div>
                     <div>
-                        <h3 class="font-bold tracking-tight sm:text-2xl my-6">
+                        <h3
+                            v-show="waitForPlayers"
+                            class="font-bold tracking-tight sm:text-2xl my-6"
+                        >
                             {{ $t('room.playersNeedToJoin') }}
                         </h3>
                         <ul
@@ -234,6 +237,9 @@ export default {
         },
         myTeam() {
             return this.teams.find((team) => team.players.some((player) => this.samePlayerCheck(player, this.getUser)));
+        },
+        waitForPlayers() {
+            return this.teams.filter((team) => team.players).length <= 1;
         },
     },
     watch: {
