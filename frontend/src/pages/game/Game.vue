2e0a8338090888de5d727fcde4fe4c97a1e26b0c
Martin Fritz Neuner <martin.neuner@student.uibk.ac.at>
2021-05-13 17:41:40 +0200

    Add game fetch when joining game

--- frontend/src/pages/game/Game.vue
+++ frontend/src/pages/game/Game.vue
@@ -3,7 +3,14 @@
 </template>
 
 <script>
+import { unsubChannel } from '@/services/websocket.service';
+import { mapActions, mapGetters } from 'vuex';
+import * as GameService from '@/services/game.service';
+import * as RoomService from '@/services/room.service';
+import * as TeamService from '@/services/team.service';
+
 export default {
+
     name: 'Game',
     props: {
         gameId: {
@@ -11,9 +18,59 @@ export default {
             required: true,
         },
     },
+    data() {
+        return {
+            game: {},
+            room: {},
+        };
+    },
+    computed: {
+        ...mapGetters({
+            getUsers: 'user/getUsers',
+            getUser: 'user/getUser',
+            topicList: 'topicList',
+        }),
+    },
+    ...mapActions({
+        fetchTopics: 'fetchTopics',
+    }),
+    mounted() {
+        if (!this.topicList) this.fetchTopics();
+        RoomService.fetchRoomById(this.$route.params.id).then((response) => {
+            this.room = response.data;
+        }).catch((error) => {
+            console.error(error);
+        });
+        let tempGame = {};
+        GameService.getGame(this.gameId).then((gameResponse) => {
+            tempGame = gameResponse.data;
+            GameService.getAllTeams(tempGame.id).then((allTeamsResponse) => {
+                tempGame.teams = allTeamsResponse.data;
+                tempGame.teams.map((team) => {
+                    const loopTeam = team;
+                    loopTeam.users = [];
+                    return loopTeam;
+                });
+                this.game = tempGame;
+                tempGame.teams.map((team) => TeamService.getTeamUsers(team.id).then((teamUsersResponse) => {
+                    const loopTeam = team;
+                    loopTeam.users = teamUsersResponse.data;
+                    return loopTeam;
+                }).catch((error) => {
+                    console.error(error);
+                }));
+            }).catch((error) => {
+                console.error(error);
+            });
+        }).catch((error) => {
+            console.error(error);
+        });
+    },
+    beforeDestroy() {
+        unsubChannel(`game/${this.gameId}`);
+    },
 };
 </script>
-
 <style scoped>
 
 </style>
