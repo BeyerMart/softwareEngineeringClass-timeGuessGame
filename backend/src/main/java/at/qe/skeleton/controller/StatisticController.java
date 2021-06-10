f5048f6ce9769c0f52aa6efd38a87cc76d0f0693
Nicolas Marte <nicolas.marte@student.uibk.ac.at>
2021-06-10 12:15:39 +0200

    Add winrate, matchhistory and last played with

---
+++ backend/src/main/java/at/qe/skeleton/controller/StatisticController.java
@@ -0,0 +1,50 @@
+package at.qe.skeleton.controller;
+
+import at.qe.skeleton.model.GameDto;
+import at.qe.skeleton.model.UserDto;
+import at.qe.skeleton.payload.response.SuccessResponse;
+import at.qe.skeleton.services.StatisticService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class StatisticController {
+    @Autowired
+    StatisticService statisticService;
+
+    @GetMapping("/statistics/winloss/{userId}")
+    private ResponseEntity<?> getWinLossRatio(@PathVariable Long userId){
+        int numberOfWins = statisticService.getNumberOfWins(userId);
+        int numberOfGames = statisticService.getNumberOfGames(userId);
+
+        float ratio = (float) numberOfWins / (float) numberOfGames;
+
+        return ResponseEntity.ok((new SuccessResponse(ratio)).toString());
+    }
+
+    @GetMapping("/statistics/matchhistory/{userId}")
+    private ResponseEntity<?> getMatchHistory(@PathVariable Long userId){
+        List<GameDto> matches = statisticService.getMatchHistory(userId);
+
+        return ResponseEntity.ok((new SuccessResponse(matches)).toString());
+    }
+
+    @GetMapping("/statistics/lastplayedwith/{userId}")
+    private ResponseEntity<?> getLastPlayedWith(@PathVariable Long userId){
+        List<UserDto> lastUsers = statisticService.getLastPlayedWith(userId);
+
+        return ResponseEntity.ok((new SuccessResponse(lastUsers)).toString());
+    }
+
+
+
+}
