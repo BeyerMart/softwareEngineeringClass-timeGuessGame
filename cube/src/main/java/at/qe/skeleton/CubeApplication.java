5c0ff029d8710eebea3c3fbad8703475a9192b7c
Martin Beyer <martin.beyer@student.uibk.ac.at>
2021-06-16 23:35:13 +0200

    Update Leave Room afer pi disconnect

--- cube/src/main/java/at/qe/skeleton/CubeApplication.java
+++ cube/src/main/java/at/qe/skeleton/CubeApplication.java
@@ -43,7 +43,7 @@ public class CubeApplication {
 
         webSocketConnection.subscribeToChannel("cube");
         webSocketConnection.sendRegistration();
-        webSocketConnection.getLogicController().startTestBackendConnection();
+        //webSocketConnection.getLogicController().sendTestConnection();
     }
 
     @PreDestroy
