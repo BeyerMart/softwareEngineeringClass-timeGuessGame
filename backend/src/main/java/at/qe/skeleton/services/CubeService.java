e6a21d8a921ba72addcb3ebabb265c9591155bd0
Martin Beyer <martin.beyer@student.uibk.ac.at>
2021-06-16 20:55:27 +0200

    Update Disconnected Pi

--- backend/src/main/java/at/qe/skeleton/services/CubeService.java
+++ backend/src/main/java/at/qe/skeleton/services/CubeService.java
@@ -3,20 +3,29 @@ package at.qe.skeleton.services;
 import at.qe.skeleton.controller.CubeController;
 import at.qe.skeleton.model.Activity;
 import at.qe.skeleton.model.Room;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class CubeService {
 
+	//piName and sessionId
 	private Map<String, String> connectedPis = new ConcurrentHashMap<String, String>();
+	private Map<String, String> lastUpdates = new ConcurrentHashMap<String, String>();
+	private final int TIMEOUT_THRESHOLD = 3;
+	private static final Logger logger = LoggerFactory.getLogger(CubeService.class);
+
+
 
 	@Autowired
 	private CubeController cubeController;
@@ -31,12 +40,15 @@ public class CubeService {
 			return false;
 		}
 		connectedPis.put(piName, sessionId);
+		lastUpdates.put(sessionId, Instant.now().toString());
 		return true;
 	}
 
 	public boolean removePi(String piName) {
+		lastUpdates.remove(connectedPis.get(piName));
 		return (connectedPis.remove(piName) != null);
 	}
+
 
 	public List<String> getAllPis() {
 		return new ArrayList<>(connectedPis.keySet());
@@ -95,5 +107,52 @@ public class CubeService {
 	public void stopBatteryNotification(int roomId) {
 		Room room = roomService.getRoomById(roomId).get();
 		cubeController.cubeStopBatteryNotification(room);
+	}
+
+	public void updateTimeouts(String sessionId, String lastHeardOf) {
+		lastUpdates.put(sessionId, lastHeardOf);
+	}
+
+	public void startCheckingForTimeouts(String sessionId) {
+		TimeoutChecker timeoutChecker = new TimeoutChecker(this, sessionId);
+		timeoutChecker.run();
+	}
+
+	class TimeoutChecker extends Thread{
+		private final String sessionId;
+		private final CubeService cubeService;
+
+		TimeoutChecker(CubeService cubeService, String sessionId){
+			this.sessionId = sessionId;
+			this.cubeService = cubeService;
+		}
+
+		public void run(){
+			boolean piIsActive = true;
+			while(piIsActive){
+				try {
+					Thread.sleep(TIMEOUT_THRESHOLD * 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (Instant.now().getEpochSecond() - Instant.parse((String)cubeService.getLastUpdates().get(sessionId)).getEpochSecond() > TIMEOUT_THRESHOLD){
+					AtomicReference<String> piNameToRemove = new AtomicReference<>("");
+					connectedPis.forEach((piName, session) -> {
+						if(sessionId.equals(session)){
+							piNameToRemove.set(piName);
+						}
+					});
+					removePi(piNameToRemove.get());
+					logger.info("Pi " + piNameToRemove + " was disconnected due to timeout.");
+					piIsActive = false;
+				}
+
+			}
+
+		}
+	}
+
+	public Map getLastUpdates(){
+		return Collections.unmodifiableMap(lastUpdates);
 	}
 }
