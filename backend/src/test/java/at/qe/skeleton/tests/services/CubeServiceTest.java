08bb2b7ddf1281374a6daaba180ea99008c38581
Martin Beyer <martin.beyer@student.uibk.ac.at>
2021-06-18 14:02:07 +0200

    Add comments for tests

--- backend/src/test/java/at/qe/skeleton/tests/services/CubeServiceTest.java
+++ backend/src/test/java/at/qe/skeleton/tests/services/CubeServiceTest.java
@@ -70,6 +70,9 @@ public class CubeServiceTest {
 		Assert.isTrue(cubeService.removePi("MyPi"));
 	}
 
+	/**
+	 * Testing getting the piNames
+	 */
 	@Test
 	public void testGetAllPis() {
 		Assert.isTrue(cubeService.addPiName("MyPi", "1234"));
@@ -82,15 +85,22 @@ public class CubeServiceTest {
 
 	}
 
+	/**
+	 * Testing getting the piNames as unmodifiable map
+	 */
 	@Test
 	public void testGetConnectedPis() {
 		Assert.isTrue(cubeService.addPiName("MyPi", "1234"));
 		Assert.isTrue(cubeService.addPiName("NotMyPi", "2345"));
 		Assertions.assertEquals(2, cubeService.getConnectedPis().size());
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> cubeService.getConnectedPis().put("abc", "342342"));
 		Assert.isTrue(cubeService.removePi("MyPi"));
 		Assert.isTrue(cubeService.removePi("NotMyPi"));
 	}
 
+	/**
+	 * Test to convert the facet data to the amount of points related to that facet
+	 */
 	@Test
 	public void testFacetToPoints() {
 		for (int i = 0; i < 12; i += 3) {
@@ -104,6 +114,9 @@ public class CubeServiceTest {
 		}
 	}
 
+	/**
+	 * Test to convert the facet data to the amount of time (in minutes) related to that facet
+	 */
 	@Test
 	public void testFacetToTime() {
 		for (int i = 0; i < 9; i++) {
@@ -114,6 +127,9 @@ public class CubeServiceTest {
 		}
 	}
 
+	/**
+	 * Test to convert the facet data to the activity related to that facet
+	 */
 	@Test
 	public void testFacetToActivity() {
 		for (int i = 0; i < 12; i++) {
@@ -129,6 +145,9 @@ public class CubeServiceTest {
 		}
 	}
 
+	/**
+	 * Test to check the updating and getting of the lastUpdates per session.
+	 */
 	@Test
 	public void testUpdateAndGetTimeouts() {
 		Instant now = Instant.now();
@@ -138,12 +157,16 @@ public class CubeServiceTest {
 		Assertions.assertEquals(now.toString(), cubeService.getLastUpdates().get("123"));
 	}
 
+	/**
+	 * Test to check the TimeoutChecker.class
+	 * This takes quite some time, as a timeout needs to occur.
+	 * The method, which sends the connection test to the pi is mocked.
+	 */
 	@Test
 	public void testTimeoutChecker() {
 		cubeService.addPiName("myTimoutPI", "123");
 		cubeService.updateTimeouts("123", Instant.now().toString());
 		doNothing().when(cubeController).cubeSendConnectionTest(new WebsocketResponse(null, WSResponseType.CONNECTION_TEST_TO_PI));
-		//Mockito.when(cubeController.cubeSendConnectionTest(new WebsocketResponse(null, WSResponseType.CONNECTION_TEST_TO_PI))).thenReturn(true);
 		cubeService.startCheckingForTimeouts("123");
 		try {
 			Thread.sleep((cubeService.getTIMEOUT_THRESHOLD() * 3 + 1) * 1000);
@@ -152,5 +175,4 @@ public class CubeServiceTest {
 		}
 		Assertions.assertEquals(0, cubeService.getConnectedPis().size());
 	}
-
 }
