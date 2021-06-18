80ce9578ea6042d6380ab605e178549ddd1bd1fb
Martin Beyer <martin.beyer@student.uibk.ac.at>
2021-06-18 21:47:31 +0200

    Add Stability Cube

--- cube/src/main/java/at/qe/skeleton/CubeApplication.java
+++ cube/src/main/java/at/qe/skeleton/CubeApplication.java
@@ -26,14 +26,15 @@ public class CubeApplication {
         cubeCalibration.calibrate();
 
         WebSocketConnection webSocketConnection = null;
+        int counter = 0;
         while (webSocketConnection == null) {
-            int counter = 0;
             try {
                 webSocketConnection = new WebSocketConnection(cubeCalibration);
-            } catch (TimeoutException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 counter++;
                 if (counter > WEBSOCKET_CONNECTION_TRIES) {
+                    System.out.println("Shutting down. No Connection to Websocket possible.");
                     System.exit(1);
                 }
                 System.out.println("A Timeout Error occurred. Trying again to connect...");
